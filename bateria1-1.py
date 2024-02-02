cbfe5c90d0a7c56d0179b72a949d47ae4a861c42
Tronexxx <158268866+Tronexxx@users.noreply.github.com>
2024-02-02 10:14:33 +0100

    Update bateria1-1.py

--- bateria1-1.py
+++ bateria1-1.py
@@ -1,4 +1,5 @@
-# num1 = int(input("Introdueix un numero: "))
+# Code Bateria1.1
+num1 = int(input("Introdueix un numero: "))
 num2 = int(input("Introdueix un altre numero: "))
 resultat = num1+num2
 print("El resultat és: ",resultat)
